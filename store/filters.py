c4b426ec6f0517d5b7f7b5fb0db6d2d1f0fe6c26
hunterbury <huntersbury1@gmail.com>
2021-12-18 21:27:52 -0500

    added widgets to filters, styled product info page and home page

--- store/filters.py
+++ store/filters.py
@@ -1,11 +1,12 @@
 import django_filters
+from django import forms
 from .models import Product, BRAND_CHOICES, TYPE_CHOICES
-from django_filters import ChoiceFilter, CharFilter
+from django_filters import ChoiceFilter, CharFilter, MultipleChoiceFilter
 
 class ProductFilter(django_filters.FilterSet):
-    brand = ChoiceFilter(field_name="brand", choices=BRAND_CHOICES, lookup_expr='icontains', label='Brand:')
-    type = ChoiceFilter(field_name="type", choices=TYPE_CHOICES, lookup_expr='icontains', label='Type:')
-    search = CharFilter(field_name="name", lookup_expr='icontains', label='')
+    brand = ChoiceFilter(field_name="brand", choices=BRAND_CHOICES, lookup_expr='icontains', label='Brand:', widget=forms.Select(attrs={'class': 'form-control'}))
+    type = ChoiceFilter(field_name="type", choices=TYPE_CHOICES, lookup_expr='icontains', label='Type:', widget=forms.Select(attrs={'class': 'form-control'}))
+    search = CharFilter(field_name="name", lookup_expr='icontains', label='', widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Product
