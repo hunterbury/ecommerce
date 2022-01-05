6993113079c58ccd548fac711efa47c28f6b953c
hunterbury <huntersbury1@gmail.com>
2022-01-05 12:46:39 -0500

    updated background, pagination styling

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,9 +1,9 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+const updateBtns = document.getElementsByClassName('update-cart')
 
 for (i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
+		let productId = this.dataset.product
+		let action = this.dataset.action
 		console.log('productId:', productId, 'Action:', action)
 		console.log('USER:', user)
 
@@ -18,7 +18,7 @@ for (i = 0; i < updateBtns.length; i++) {
 function updateUserOrder(productId, action){
 	console.log('User is authenticated, sending data...')
 
-		var url = '/update-item/'
+		let url = '/update-item/'
 
 		fetch(url, {
 			method:'POST',
