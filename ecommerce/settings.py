d221b6118c9284ee76c71aadf336b1fbcb2e5e5f
hunterbury <huntersbury1@gmail.com>
2022-02-04 14:05:42 -0500

    setup heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ds6j*_(aon)5mq#xpo%2c^%t%^w8tu2etpr+=y2)@ruqnb2v(@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hunterbury-ecommerce.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
