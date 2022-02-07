9fa826bb5425972d803715f88fb74006aa3b09aa
hunterbury <huntersbury1@gmail.com>
2022-02-07 12:59:48 -0500

    cleaned code

--- store/models.py
+++ store/models.py
@@ -37,25 +37,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-# class Category(models.Model):
-#     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=200, blank=True, null=True)
-#     slug = models.SlugField(blank=True, null=True)
-#     parent = models.ForeignKey('self',blank=True, null=True, related_name='children', on_delete=models.SET_NULL)
-
-#     class Meta:
-#         unique_together = ('slug', 'parent',)    
-#         verbose_name_plural = "categories"     
-
-#     def __str__(self):                           
-#         full_path = [self.name]                  
-#         k = self.parent
-#         while k is not None:
-#             full_path.append(k.name)
-#             k = k.parent
-#         return ' -> '.join(full_path[::-1])
-
-
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
     images = models.URLField()
